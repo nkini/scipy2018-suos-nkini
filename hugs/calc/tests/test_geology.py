7f4a2f2d4111a54257aa0ca8bb53d111c568748e
Nikhil Kini <nkini@ucsc.edu>
2018-07-10 10:05:03 -0500

    Added more tests

--- hugs/calc/tests/test_geology.py
+++ hugs/calc/tests/test_geology.py
@@ -4,8 +4,15 @@ from hugs.calc import snell_angle
 
 from numpy.testing import assert_almost_equal
 
+import pytest
 
 def test_snell():
     """Test the basic wind component calculation."""
     res = snell_angle(12, 2500, 4000)
     assert_almost_equal(res, 19.43022, 4)
+
+
+def test_exception_snell_angle():
+    """Test that warning is raised when wind direction > 360."""
+    with pytest.raises(ValueError):
+        snell_angle(3, 0, 12)
--- hugs/calc/tests/test_met.py
+++ hugs/calc/tests/test_met.py
@@ -1,8 +1,9 @@
 """Test the `met` module."""
 
-from hugs.calc import get_wind_dir, get_wind_speed
+from hugs.calc import get_wind_dir, get_wind_speed, get_wind_components, snell_angle
 
 import numpy as np
+import pytest
 from numpy.testing import assert_almost_equal, assert_array_almost_equal
 
 
@@ -35,3 +36,29 @@ def test_dir():
     true_dir = np.array([270., 225., 180., 270.])
 
     assert_array_almost_equal(true_dir, direc, 4)
+
+
+def test_scalar_components():
+    """Test calculating wind components with scalars"""
+    components = get_wind_components(150, 0)
+    assert_almost_equal(components, (0, -150))
+    components = get_wind_components(-150, 90)
+    assert_almost_equal(components, (150, 0))
+    components = get_wind_components(10, 30)
+    assert_almost_equal(components, (-5, -8.660254))
+    components = get_wind_components(20, 45)
+    assert_almost_equal(components, (-14.1421356, -14.1421356))
+    
+
+def test_vector_components():
+    """Test calculating wind components with vectors"""
+    components = get_wind_components(np.array([150, -150, 10, 20]), np.array([0, 90, 30, 45]))
+    results_should_be = [[0, 150, -5, -14.1421356],
+                         [-150, 0, -8.660254, -14.1421356]]
+    assert_array_almost_equal(results_should_be, components)
+
+
+def test_warning_direction():
+    """Test that warning is raised when wind direction > 360."""
+    with pytest.warns(UserWarning):
+        get_wind_components(3, 460)
